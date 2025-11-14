16c6510e62d33819bf9bcdd98e30110121d21112
EDP-10\edp <mafudmr5@gmail.com>
2025-11-14 21:00:03 +0700

    update Image2

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-white bg-opacity-90 shadow-md">
       <div className="text-3xl cursor-pointer">
         <div className="space-y-1">
           <a href="/">
-            <img src="/images/logo_baru_2.jpg" width="60" height="60" alt="Logo" />
+            <Image src="/images/logo_baru_2.jpg" width="60" height="60" alt="Logo" />
           </a>
         </div>
       </div>
