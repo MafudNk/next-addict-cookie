a48a7f074db43fc9e9d14e25f0f1a7d2246887e9
EDP-10\edp <mafudmr5@gmail.com>
2025-11-16 12:27:05 +0700

    update Link

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,19 +1,20 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between px-6 py-4 bg-white bg-opacity-90 shadow-md">
       <div className="text-3xl cursor-pointer">
         <div className="space-y-1">
-          <a href="/">
+          <Link href="/">
             <Image src="/images/logo_baru_2.jpg" width="60" height="60" alt="Logo" />
-          </a>
+          </Link>
         </div>
       </div>
       <ul className="flex space-x-10 text-lg font-semibold text-black">
-        <li><a href="/produk" className="hover:text-gray-600">Produk</a></li>
-        <li><a href="/order" className="hover:text-gray-600">Order</a></li>
-        <li><a href="#" className="hover:text-gray-600">Lokasi</a></li>
+        <li><Link href="/produk" className="hover:text-gray-600">Produk</Link></li>
+        <li><Link href="/order" className="hover:text-gray-600">Order</Link></li>
+        <li><Link href="#" className="hover:text-gray-600">Lokasi</Link></li>
       </ul>
     </nav>
   );
